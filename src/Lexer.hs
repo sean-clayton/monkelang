7e7e10820735d2965f84986a80b7bb1a22853ac8
🎷🐢 S. P. O. Clayton <yo@sean.wtf>
2023-09-17 19:14:08 -0400

    Lexing works!!

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,52 +1,56 @@
-module Lexer (Lexer, run, nextToken, tokenFromChar) where
+module Lexer (Lexer, make, nextToken) where
 
-import Data.Char (isLetter, isNumber, isSpace)
+import Data.Char (isDigit, isLetter, isSpace)
 import Text.Printf (printf)
 import Token qualified
 
 data Lexer = Lexer
   { input :: String,
     position :: Int,
-    ch :: Maybe Char,
-    tokens :: [Token.Token]
+    ch :: Maybe Char
   }
   deriving (Show, Eq)
 
 {- Create a new lexer from an input -}
-run :: String -> Lexer
-run input =
+make :: String -> Lexer
+make input =
   if null input
-    then Lexer {input, position = 0, ch = Nothing, tokens = []}
-    else Lexer {input, position = 0, ch = Just (head input), tokens = []}
-
-tokenFromChar ';' = Token.Semicolon
-tokenFromChar '(' = Token.LeftParen
-tokenFromChar ')' = Token.RightParen
-tokenFromChar ',' = Token.Comma
-tokenFromChar '+' = Token.Plus
-tokenFromChar '-' = Token.Minus
-tokenFromChar '/' = Token.Slash
-tokenFromChar '*' = Token.Asterisk
-tokenFromChar '<' = Token.LessThan
-tokenFromChar '>' = Token.GreaterThan
-tokenFromChar '{' = Token.LeftBrace
-tokenFromChar '}' = Token.RightBrace
-tokenFromChar char = error $ printf "unknown char: %c" char
+    then Lexer {input, position = 0, ch = Nothing}
+    else Lexer {input, position = 0, ch = Just (head input)}
 
 {- Move the lexer to the next token.
    Returns the new lexer after moving, and maybe a token. -}
-nextToken lexer =
-  case ch lexer of
-    Just currentChar -> (advance lexer, Just (tokenFromChar currentChar))
-    Nothing -> (lexer, Nothing)
+nextToken dirtyLexer =
+  let lexer = skipWhitespace dirtyLexer
+   in case ch lexer of
+        Nothing -> (lexer, Nothing)
+        Just currentChar ->
+          let (newLexer, token) = case currentChar of
+                ';' -> (advance lexer, Token.Semicolon)
+                '(' -> (advance lexer, Token.LeftParen)
+                ')' -> (advance lexer, Token.RightParen)
+                ',' -> (advance lexer, Token.Comma)
+                '+' -> (advance lexer, Token.Plus)
+                '-' -> (advance lexer, Token.Minus)
+                '/' -> (advance lexer, Token.Slash)
+                '*' -> (advance lexer, Token.Asterisk)
+                '<' -> (advance lexer, Token.LessThan)
+                '>' -> (advance lexer, Token.GreaterThan)
+                '{' -> (advance lexer, Token.LeftBrace)
+                '}' -> (advance lexer, Token.RightBrace)
+                '!' -> ifPeeked '=' Token.NotEqual Token.Bang lexer
+                '=' -> ifPeeked '=' Token.Equal Token.Assign lexer
+                char | isDigit char -> readNumber lexer
+                char | isIdentifier char -> readIdentifier lexer
+                char -> error $ printf "unknown char: %c" char
+           in (newLexer, Just token)
 
 advance lexer =
   if position lexer >= length (input lexer) - 1
     then lexer {ch = Nothing}
     else
-      ( let newPosition = position lexer + 1
-         in lexer {position = newPosition, ch = Just (input lexer !! newPosition)}
-      )
+      let newPosition = position lexer + 1
+       in lexer {position = newPosition, ch = Just (input lexer !! newPosition)}
 
 seekLoop lexer condition =
   if condition $ ch lexer
@@ -61,20 +65,31 @@ skipWhitespace lexer =
   let (newLexer, _) = seek lexer (maybe False isSpace)
    in newLexer
 
-substring start len s = take len (drop start s)
+substr start len s = take len (drop start s)
+
+peekChar lexer =
+  if position lexer >= length (input lexer) - 1
+    then Nothing
+    else Just (input lexer !! (position lexer + 1))
+
+ifPeeked char matchToken defaultToken lexer =
+  let (newLexer, token) = case peekChar lexer of
+        Just peekedChar | peekedChar == char -> (advance lexer, matchToken)
+        _ -> (lexer, defaultToken)
+   in (newLexer, token)
 
 readWhile lexer condition =
   let posStart = position lexer
       (newLexer, posEnd) = seek lexer (maybe False condition)
-      readLength = posEnd - posStart
-   in (newLexer, substring posStart readLength (input lexer))
+      readLength = posEnd - posStart + 1
+   in (newLexer, substr posStart readLength (input lexer))
 
 readIdentifier lexer =
   let (newLexer, ident) = readWhile lexer isIdentifier
    in (newLexer, Token.lookupIdent ident)
 
 readNumber lexer =
-  let (newLexer, int) = readWhile lexer isNumber
+  let (newLexer, int) = readWhile lexer isDigit
    in (newLexer, Token.Integer int)
 
 isIdentifier = isLetter
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,18 +3,132 @@ import Test.Hspec
 import Token qualified
 
 main = hspec $ do
-  describe "Lexer.tokenFromChar" $ do
-    it "correctly lexes a ';'" $ do
-      Lexer.tokenFromChar ';' `shouldBe` Token.Semicolon
-    it "correctly lexes a '+'" $ do
-      Lexer.tokenFromChar '+' `shouldBe` Token.Plus
-    it "correctly lexes a '-'" $ do
-      Lexer.tokenFromChar '-' `shouldBe` Token.Minus
-    it "correctly lexes a '('" $ do
-      Lexer.tokenFromChar '(' `shouldBe` Token.LeftParen
-    it "correctly lexes a ')'" $ do
-      Lexer.tokenFromChar ')' `shouldBe` Token.RightParen
-    it "correctly lexes a '{'" $ do
-      Lexer.tokenFromChar '{' `shouldBe` Token.LeftBrace
-    it "correctly lexes a '}'" $ do
-      Lexer.tokenFromChar '}' `shouldBe` Token.RightBrace
+  describe "Lexer.nextToken" $ do
+    it "correctly creates empty lexer from an empty string" $ do
+      let lexer = Lexer.make ""
+       in Lexer.nextToken lexer `shouldBe` (lexer, Nothing)
+
+    it "correctly lexes ';' as Semicolon" $ do
+      let lexer = Lexer.make ";"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.Semicolon
+
+    it "correctly lexes '(' as LeftParen" $ do
+      let lexer = Lexer.make "("
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.LeftParen
+
+    it "correctly lexes ')' as RightParen" $ do
+      let lexer = Lexer.make ")"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.RightParen
+
+    it "correctly lexes ',' as Comma" $ do
+      let lexer = Lexer.make ","
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.Comma
+
+    it "correctly lexes '+' as Plus" $ do
+      let lexer = Lexer.make "+"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.Plus
+
+    it "correctly lexes '-' as Minus" $ do
+      let lexer = Lexer.make "-"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.Minus
+
+    it "correctly lexes '/' as Slash" $ do
+      let lexer = Lexer.make "/"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.Slash
+
+    it "correctly lexes '*' as Asterisk" $ do
+      let lexer = Lexer.make "*"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.Asterisk
+
+    it "correctly lexes '<' as LessThan" $ do
+      let lexer = Lexer.make "<"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.LessThan
+
+    it "correctly lexes '>' as GreaterThan" $ do
+      let lexer = Lexer.make ">"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.GreaterThan
+
+    it "correctly lexes '{' as LeftBrace" $ do
+      let lexer = Lexer.make "{"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.LeftBrace
+
+    it "correctly lexes '}' as RightBrace" $ do
+      let lexer = Lexer.make "}"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.RightBrace
+
+    it "correctly lexes '!' as Bang" $ do
+      let lexer = Lexer.make "!"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.Bang
+
+    it "correctly lexes '=' as Assign" $ do
+      let lexer = Lexer.make "="
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.Assign
+
+    it "correctly lexes '!=' as NotEqual" $ do
+      let lexer = Lexer.make "!="
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.NotEqual
+
+    it "correctly lexes '==' as Equal" $ do
+      let lexer = Lexer.make "=="
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.Equal
+
+    it "correctly lexes '1234' as Integer 1234" $ do
+      let lexer = Lexer.make "1234"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just (Token.Integer "1234")
+
+    it "correctly lexes 'fn' as Function" $ do
+      let lexer = Lexer.make "fn"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.Function
+
+    it "correctly lexes 'let' as Let" $ do
+      let lexer = Lexer.make "let"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.Let
+
+    it "correctly lexes 'true' as True" $ do
+      let lexer = Lexer.make "true"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.True
+
+    it "correctly lexes 'false' as False" $ do
+      let lexer = Lexer.make "false"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.False
+
+    it "correctly lexes 'if' as If" $ do
+      let lexer = Lexer.make "if"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.If
+
+    it "correctly lexes 'else' as Else" $ do
+      let lexer = Lexer.make "else"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.Else
+
+    it "correctly lexes 'return' as Return" $ do
+      let lexer = Lexer.make "return"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just Token.Return
+
+    it "correctly lexes 'asdf' as Ident asdf" $ do
+      let lexer = Lexer.make "asdf"
+          (_, token) = Lexer.nextToken lexer
+       in token `shouldBe` Just (Token.Ident "asdf")
